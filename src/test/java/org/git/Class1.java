e88048597b13775b50291e697f0769f642fab9be
DhanalakshmiMurugan02 <dhanalakshmi.murugan@gmail.com>
2023-02-11 15:08:28 +0530

    3 methods added to Red

--- src/test/java/org/git/Class1.java
+++ src/test/java/org/git/Class1.java
@@ -10,4 +10,13 @@ System.out.println("2");
 private void method3() {
 System.out.println("3");
 }
+private void tamilNadu() {
+System.out.println("4");
+}
+private void kerala() {
+System.out.println("5");
+}
+private void banglore() {
+System.out.println("6");
+}
 }
